06240a0db0e23d876be41eee5859accaf9ba2820
maccchan4869 <ymatsio554@gmail.com>
2020-10-19 16:14:49 +0900

    ユーザー画面ファイル追加

---
+++ db/migrate/20201018225331_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[6.0]
+  def change
+    create_table :users do |t|
+      t.string :login_id, null: false
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :password_digest, null: false
+      t.boolean :admin, null: false, default: false
+
+      t.timestamps
+      t.index :login_id, unique: true
+      t.index :email, unique: true
+    end
+  end
+end
