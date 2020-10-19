e12b4cacf2194d59f805e6e616171e9c14622144
maccchan4869 <ymatsuo554@gmail.com>
2020-10-19 16:20:29 +0900

    ユーザー管理画面実装

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,6 @@
 class Admin::UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def edit
@@ -9,5 +10,43 @@ class Admin::UsersController < ApplicationController
   end
 
   def index
+    @users = User.all
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to admin_user_path(@user), notice: "ユーザ「#{@user.name}」を登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to admin_user_path(@user), notice: "ユーザ「#{@user.name}」を更新しました。"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザ「#{@user.name}」を削除しました。"
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def require_admin
+    redirect_to root_path unless current_user.admin?
   end
 end
