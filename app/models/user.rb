e82443725da99fb1585fd85077c3d07a64089472
maccchan4869 <ymatsio554@gmail.com>
2020-10-20 23:09:55 +0900

    ユーザーモデルに検証機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
+
+    validates :login_id, presence: true, uniqueness: true
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
 end
