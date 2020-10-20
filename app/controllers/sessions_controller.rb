da5581f26f0ae7df8230a99bd13c3ad9f775623a
maccchan4869 <ymatsio554@gmail.com>
2020-10-20 11:08:27 +0900

    ログイン機能追加

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+  skip_before_action :login_required
+  
+  def new
+  end
+  
+  def create
+      user = User.find_by(login_id: session_params[:login_id])
+      
+      if user&.authenticate(session_params[:password])
+          session[:user_id] = user.id
+          redirect_to root_url, notice: 'ログインしました'
+      else
+          render :new
+      end
+  end
+  
+  def destroy
+      reset_session
+      redirect_to root_url, notice: 'ログアウトしました'
+  end
+  
+  private
+  
+  def session_params
+      params.require(:session).permit(:login_id, :password)
+  end
+end
